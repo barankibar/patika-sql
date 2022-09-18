a8559d4cc777016a15225ed1c5d2d8ec0923d7a0
barankibar <56038362+barankibar@users.noreply.github.com>
2022-09-19 01:20:11 +0300

    task-3

---
+++ task-3/task-3.sql
@@ -0,0 +1,15 @@
+-- select country from country 
+
+-- where country like 'A%' and country like '%a';
+
+-- select country from country 
+
+-- where country like '______%n';
+
+-- select title from film 
+
+-- where title ilike '%T%t%t%t%';
+
+-- select title from film 
+
+-- where title like 'C%' and length > 90 and rental_rate = 2.99;
