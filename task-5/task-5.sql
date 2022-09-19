3d59036889e8c67ec089092f177f0b2462bcff5d
barankibar <56038362+barankibar@users.noreply.github.com>
2022-09-19 15:07:38 +0300

    task-6

---
+++ task-5/task-5.sql
@@ -0,0 +1,17 @@
+-- select title from film 
+-- where title like '%n'
+-- order by length desc
+-- limit 5;
+-- ---------------------------------
+
+-- select title from film 
+-- where title like '%n'
+-- order by length asc
+-- offset 5
+-- limit 5;
+-- ---------------------------------
+
+-- select last_name from customer
+-- where store_id = 1
+-- order by last_name asc 
+-- limit 4;
