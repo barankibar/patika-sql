743835416d036d685e93111a2e3ebde908bb0ebf
barankibar <56038362+barankibar@users.noreply.github.com>
2022-09-19 22:16:18 +0300

    task-9

---
+++ task-9/task-9.sql
@@ -0,0 +1,10 @@
+-- select city,country from city 
+-- inner join country on country.country_id = city.country_id;
+-- ------------------------------------------------------------------
+
+-- SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+-- INNER JOIN payment ON payment.customer_id = customer.customer_id;
+-- ------------------------------------------------------------------
+
+-- SELECT rental_id, first_name, last_name FROM customer
+-- INNER JOIN rental ON rental.customer_id = customer.customer_id;
