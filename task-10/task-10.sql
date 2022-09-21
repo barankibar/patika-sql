ea567c9164be007c3a794ff752cfa88e7130ca1f
barankibar <56038362+barankibar@users.noreply.github.com>
2022-09-21 16:23:02 +0300

    task-10

---
+++ task-10/task-10.sql
@@ -0,0 +1,14 @@
+-- select city, country from city
+
+-- left join country on country.country_id = city.country_id;
+
+-- -----------------------------------------------------------------------
+-- select first_name, last_name from customer 
+
+-- right join payment on payment.customer_id = customer.customer_id;
+
+-- -----------------------------------------------------------------------
+
+-- select first_name, last_name from customer 
+
+-- full join rental on rental.customer_id = customer.customer_id;
